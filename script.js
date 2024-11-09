73cdd20be13749e316bce72aa7668a4c21d37370
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2024-11-09 14:18:00 +0100

    Add function to compute an expression

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const expression = {
+    firstOperand: null,
+    operator: null,
+    secondOperand: null
+}
+
+function operate(operator, firstOperand, secondOperand) {
+    switch (operator) {
+        case '+': return add(firstOperand, secondOperand);
+        case '-': return subtract(firstOperand, secondOperand);
+        case '*': return multiply(firstOperand, secondOperand);
+        case '/': return divide(firstOperand, secondOperand);
+    }
+}
+
 function add(firstOperand, secondOperand) {
     return firstOperand + secondOperand;
 }
@@ -11,5 +26,6 @@ function multiply(firstOperand, secondOperand) {
 }
 
 function divide(firstOperand, secondOperand) {
+    if (secondOperand === 0) return 'Cannot divide by 0!';
     return firstOperand / secondOperand;
 }
