8ee18ebf5e4124371218084c62a4269a30f5b13d
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2024-11-09 20:43:24 +0100

    Add reactivity to user actions

--- script.js
+++ script.js
@@ -1,7 +1,121 @@
-const expression = {
-    firstOperand: null,
-    operator: null,
-    secondOperand: null
+let state = {
+    expression: {
+        firstOperand: null,
+        operator: null,
+        secondOperand: null
+    },
+    displayValue: '',
+    freeze: false
+}
+
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('#display');
+
+calculator.addEventListener('click', event => {
+    switch (event.target.id) {
+        case '0':
+            updateDisplayValue('0');
+            break;
+        case '1':
+            updateDisplayValue('1');
+            break;
+        case '2':
+            updateDisplayValue('2');
+            break;
+        case '3':
+            updateDisplayValue('3');
+            break;
+        case '4':
+            updateDisplayValue('4');
+            break;
+        case '5':
+            updateDisplayValue('5');
+            break;
+        case '6':
+            updateDisplayValue('6');
+            break;
+        case '7':
+            updateDisplayValue('7');
+            break;
+        case '8':
+            updateDisplayValue('8');
+            break;
+        case '9':
+            updateDisplayValue('9');
+            break;
+        case '+':
+            computeExpression('+');
+            break;
+        case '-':
+            computeExpression('-');
+            break;
+        case '*':
+            computeExpression('*');
+            break;
+        case '/':
+            computeExpression('/');
+            break;
+        case '=':
+            computeExpression('=');
+            break;
+        case 'clear':
+        case 'clearText':
+            clear();
+            break;
+    }
+
+    updateExpression();
+    updateDisplay();
+});
+
+
+function computeExpression(operator) {
+    if (state.expression.firstOperand === null) return;
+    state.freeze = true;
+    if (state.expression.firstOperand !== null && state.expression.operator !== null && state.expression.secondOperand !== null) {
+        state.expression.firstOperand = operate(state.expression.operator, state.expression.firstOperand, state.expression.secondOperand);
+        if (operator === '=') {
+            state.expression.operator = null;
+        } else {
+            state.expression.operator = operator;
+        }
+        state.expression.secondOperand = null;
+        state.displayValue = state.expression.firstOperand;
+    } else if (state.expression.firstOperand !== null && state.expression.operator === null) {
+        state.expression.operator = operator;
+    }
+}
+
+function updateExpression() {
+    if (state.freeze || state.displayValue === '') return;
+    const number = Number(state.displayValue);
+    if (state.expression.operator === null) {
+        state.expression.firstOperand = number;
+    } else {
+        state.expression.secondOperand = number;
+    }
+}
+
+function updateDisplayValue(value) {
+    if (state.expression.firstOperand !== null && state.expression.operator !== null && state.expression.secondOperand === null) {
+        state.displayValue = value;
+    } else {
+        state.displayValue += value;
+    }
+}
+
+function updateDisplay() {
+    console.log(state)
+    state.freeze = false;
+    display.innerText = state.displayValue;
+}
+
+function clear() {
+    state.expression.firstOperand = null;
+    state.expression.operator = null;
+    state.expression.secondOperand = null;
+    state.displayValue = '';
+    state.freeze = false;
 }
 
 function operate(operator, firstOperand, secondOperand) {
@@ -26,6 +140,5 @@ function multiply(firstOperand, secondOperand) {
 }
 
 function divide(firstOperand, secondOperand) {
-    if (secondOperand === 0) return 'Cannot divide by 0!';
     return firstOperand / secondOperand;
 }
