dd63d2aa39d537365faf34c90ce6de38b064dfc0
Maximilien Ledoux <80272454+MaxLedx@users.noreply.github.com>
2024-11-10 20:46:38 +0100

    Refactor (#1)

--- script.js
+++ script.js
@@ -1,91 +1,123 @@
-let state = {
-    expression: {
-        firstOperand: null,
-        operator: null,
-        secondOperand: null
-    },
+const state = {
+    expression: { a: null, operator: null, b: null },
     displayValue: '',
-    freeze: false
-}
+    forceClear: false,
+};
 
-const OPERATORS = ['+', '-', '*', '/', '='];
-const calculator = document.querySelector('#calculator');
-const display = document.querySelector('#display');
+const calculator = {
+    operators: ['plus', 'minus', 'times', 'divide', 'equals'],
+    operations: {
+        plus: (a, b) => a + b,
+        minus: (a, b) => a - b,
+        times: (a, b) => a * b,
+        divide: (a, b) => {
+            if (b === 0) throw new RangeError('MATH ERROR');
+            return a / b;
+        },
+    },
+};
 
-calculator.addEventListener('click', event => {
+const ui = {
+    calculator: document.querySelector('#calculator'),
+    display: document.querySelector('#displayText'),
+    decimal: document.querySelector('#decimal'),
+    backspace: document.querySelector('#backspace'),
+    operators: ['plus', 'minus', 'times', 'divide'].reduce((acc, op) => {
+        acc[op] = document.querySelector(`#${op}`);
+        return acc;
+    }, {})
+};
+
+ui.calculator.addEventListener('click', event => {
+    if (state.forceClear) onClearHandler();
+
     const id = event.target.id;
-    if (!isNaN(id)) updateDisplayValue(id);
-    else if (OPERATORS.includes(id)) computeExpression(id);
-    else if (id === 'clear' || id === 'clearText') clear();
-    updateExpression();
-    updateDisplay();
+
+    if (!isNaN(id)) onDigitInputHandler(id);
+    else if (calculator.operators.includes(id)) onOperatorInputHandler(id);
+    else if (id === 'clear' || id === 'clearText') onClearHandler();
+    else if (id === 'decimal') onDigitInputHandler('.');
+    else if (id === 'backspace') onBackspaceHandler();
+
+    dumpState();
+    renderView();
 });
 
-function computeExpression(operator) {
-    if (state.expression.firstOperand === null) return;
-    state.freeze = true;
-    if (state.expression.firstOperand !== null && state.expression.operator !== null && state.expression.secondOperand !== null) {
-        state.expression.firstOperand = operate(state.expression.operator, state.expression.firstOperand, state.expression.secondOperand);
-        state.expression.operator = operator === '=' ? null : operator;
-        state.expression.secondOperand = null;
-        state.displayValue = state.expression.firstOperand;
-    } else if (state.expression.firstOperand !== null && state.expression.operator === null) {
-        state.expression.operator = operator;
+function onOperatorInputHandler(operator) {
+    const expression = state.expression;
+    if (expression.a !== null && expression.operator !== null && expression.b !== null) {
+        try {
+            expression.a = calculator.operations[expression.operator](expression.a, expression.b);
+            expression.operator = null;
+            expression.b = null;
+            state.displayValue = round(expression.a).toString();
+        } catch (error) {
+            state.displayValue = error.message;
+            state.forceClear = true;
+        }
+    }
+    if (expression.a !== null && expression.operator === null && operator !== 'equals') {
+        expression.operator = operator;
     }
 }
 
-function updateExpression() {
-    if (state.freeze || state.displayValue === '') return;
-    const number = Number(state.displayValue);
-    if (state.expression.operator === null) {
-        state.expression.firstOperand = number;
+function onDigitInputHandler(digit) {
+    if (digit === '.' && isDecimalDisabled()) return;
+    const expression = state.expression;
+    if (expression.operator === null) {
+        updateDisplayValue(digit, expression.a === null);
+        expression.a = Number(state.displayValue);
     } else {
-        state.expression.secondOperand = number;
+        updateDisplayValue(digit, expression.b === null);
+        expression.b = Number(state.displayValue);
     }
 }
 
-function updateDisplayValue(value) {
-    if (state.expression.firstOperand !== null && state.expression.operator !== null && state.expression.secondOperand === null) {
-        state.displayValue = value;
+function onBackspaceHandler() {
+    if (isBackspaceDisabled()) return;
+    state.displayValue = state.displayValue.slice(0, -1) || '';
+    if (state.expression.operator) {
+        state.expression.b = Number(state.displayValue) || null;
     } else {
-        state.displayValue += value;
+        state.expression.a = Number(state.displayValue) || null;
     }
 }
 
-function updateDisplay() {
-    state.freeze = false;
-    display.innerText = state.displayValue;
+function updateDisplayValue(value, reset) {
+    state.displayValue = reset ? value : state.displayValue + value;
 }
 
-function clear() {
-    state.expression.firstOperand = null;
+function isDecimalDisabled() {
+    const { displayValue, expression } = state;
+    return !displayValue || displayValue.includes('.') || (expression.operator && expression.b === null);
+}
+
+function isBackspaceDisabled() {
+    return state.expression.operator && state.expression.b === null;
+}
+
+function renderView() {
+    ui.display.innerText = state.displayValue;
+    ui.decimal.classList.toggle('disabled', isDecimalDisabled());
+    ui.backspace.classList.toggle('disabled', isBackspaceDisabled());
+    Object.values(ui.operators).forEach(op => op.classList.remove('highlight'));
+    if (state.expression.operator in ui.operators) ui.operators[state.expression.operator].classList.add('highlight');
+}
+
+function onClearHandler() {
+    state.expression.a = null;
     state.expression.operator = null;
-    state.expression.secondOperand = null;
+    state.expression.b = null;
     state.displayValue = '';
-    state.freeze = false;
+    state.forceClear = false;
 }
 
-function operate(operator, firstOperand, secondOperand) {
-    switch (operator) {
-        case '+': return add(firstOperand, secondOperand);
-        case '-': return subtract(firstOperand, secondOperand);
-        case '*': return multiply(firstOperand, secondOperand);
-        case '/': return divide(firstOperand, secondOperand);
-    }
+// Use for debug
+function dumpState() {
+    console.log(state);
 }
 
-function add(firstOperand, secondOperand) {
-    return firstOperand + secondOperand;
-}
-
-function subtract(firstOperand, secondOperand) {
-    return firstOperand - secondOperand;
-}
-
-function multiply(firstOperand, secondOperand) {
-    return firstOperand * secondOperand;
-}
-
-function divide(firstOperand, secondOperand) {
-    return firstOperand / secondOperand;
+// https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary
+function round(number) {
+    return Math.round((number + Number.EPSILON) * 100) / 100;
 }
