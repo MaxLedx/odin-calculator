1442a754c19ec20f45deb2ee9d49ba04005e5286
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-04-29 14:59:09 +0200

    feat: add operate function

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ function multiply(leftOperand, rightOperand) {
 function divide(leftOperand, rightOperand) {
     return leftOperand / rightOperand;
 }
+
+function operate(operator, leftOperand, rightOperand) {
+    switch (operator) {
+        case '+': return add(leftOperand, rightOperand);
+        case '-': return subtract(leftOperand, rightOperand);
+        case '*': return multiply(leftOperand, rightOperand);
+        case '/': return divide(leftOperand, rightOperand);
+    }
+}
