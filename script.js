79541f61c2a6e9bd130e93ffa1d253ef225bb0d1
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-04-29 14:56:51 +0200

    feat: add basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello World!');
+
+function add(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
+}
+
+function subtract(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
+}
+
+function multiply(leftOperand, rightOperand) {
+    return leftOperand * rightOperand;
+}
+
+function divide(leftOperand, rightOperand) {
+    return leftOperand / rightOperand;
+}
